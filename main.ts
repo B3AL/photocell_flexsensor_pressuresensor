74b0935276c2ec65e92d053567339cf48ac46205
B3AL <92798364+B3AL@users.noreply.github.com>
2021-11-19 09:34:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 let value = 0
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
-    basic.showNumber(value)
+    if (value > 400) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showNumber(value)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showNumber(value)
+    }
 })
